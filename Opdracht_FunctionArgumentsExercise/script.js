429dcae6eaac972814c77169c290468d5e4e38b2
Yasmin <88096861+yasminevans@users.noreply.github.com>
2021-08-04 15:55:54 +0200

    Add files via upload

---
+++ Opdracht_FunctionArgumentsExercise/script.js
@@ -0,0 +1,14 @@
+/* const painting = function (color) {
+    console.log("The wall has been painted " + color);
+}
+
+painting("red");
+painting("green");
+painting("yellow");
+painting();
+ */
+const painting = function (whichWall, color) {
+    console.log("The " + whichWall, "has been painted " + color);
+}
+
+painting("south-east wall", "red");
